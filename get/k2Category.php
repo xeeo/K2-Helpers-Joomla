0a6dde413288557d25df930c448592cd5c3b2fa1
xeeo <geana.raul@gmail.com>
2015-06-15 14:26:53 +0300

    Improvements

--- get/k2Category.php
+++ get/k2Category.php
@@ -23,49 +23,82 @@ class XeeoGetK2Category extends XeeoCoreSingleton
     }
 
     public function getRawK2Category($k2CategoryId) {
-        $row = JTable::getInstance('K2Category', 'Table');
-        $row->load($k2CategoryId);
 
-        return $row;
-    }
-
-    public function getK2Categroy($k2CategoryId) {
-
-        $user  = JFactory::getUser();
-        $aid   = (int)$user->get('aid');
         $id    = (int)$k2CategoryId;
         $db    = JFactory::getDBO();
-        $query = "SELECT * FROM #__k2_categories WHERE id={$id} AND published=1 AND trash=0 AND access<={$aid} ";
+        $query = "SELECT * FROM #__k2_categories WHERE id={$id} ORDER BY ordering ASC";
 
         $db->setQuery($query);
 
         $row = $db->loadObject();
+        $row->link = urldecode(JRoute::_(K2HelperRoute::getCategoryRoute($row->id.':'.urlencode($row->alias))));
 
         return $row;
     }
 
-    public function getK2SubCategroies($k2CategoryId) {
+    public function getK2Category($k2CategoryId) {
 
-        $user  = JFactory::getUser();
-        $aid   = (int)$user->get('aid');
+        $mainframe = JFactory::getApplication();
+        $user      = JFactory::getUser();
+        $aid       = (int)$user->get('aid');
+
         $id    = (int)$k2CategoryId;
         $db    = JFactory::getDBO();
-        $query = "SELECT * FROM #__k2_categories WHERE parent={$id} AND published=1 AND trash=0 AND access<={$aid} ";
+        $query = "SELECT * FROM #__k2_categories WHERE id={$id}";
+
+        $query .= " AND published=1  AND trash=0";
+        if (K2_JVERSION != '15') {
+            $query .= " AND access IN(" . implode(',', $user->getAuthorisedViewLevels()) . ")";
+            if ($mainframe->getLanguageFilter()) {
+                $query .= " AND language IN(" . $db->Quote(JFactory::getLanguage()->getTag()) . ", " . $db->Quote('*') . ")";
+            }
+        } else {
+            $query .= " AND access<={$aid}";
+        }
+
+        $query .= " ORDER BY ordering ASC";
+
+        $db->setQuery($query);
+
+        $row = $db->loadObject();
+        $row->link = urldecode(JRoute::_(K2HelperRoute::getCategoryRoute($row->id.':'.urlencode($row->alias))));
+
+        return $row;
+    }
+
+    public function getK2SubCategories($k2CategoryId) {
+
+        $mainframe = JFactory::getApplication();
+        $user      = JFactory::getUser();
+        $aid       = (int)$user->get('aid');
+
+        $id    = (int)$k2CategoryId;
+        $db    = JFactory::getDBO();
+        $query = "SELECT * FROM #__k2_categories WHERE parent={$id}";
+
+        $query .= " AND published=1  AND trash=0";
+        if (K2_JVERSION != '15') {
+            $query .= " AND access IN(" . implode(',', $user->getAuthorisedViewLevels()) . ")";
+            if ($mainframe->getLanguageFilter()) {
+                $query .= " AND language IN(" . $db->Quote(JFactory::getLanguage()->getTag()) . ", " . $db->Quote('*') . ")";
+            }
+        } else {
+            $query .= " AND access<={$aid}";
+        }
+
+        $query .= " ORDER BY ordering ASC";
 
         $db->setQuery($query);
 
         $rows = $db->loadObjectList();
 
-        return $rows;
-    }
+        $categoryList = array();
 
-    public function get($k2CategoryId) {
+        foreach ($rows as $row) {
+            $row->link = urldecode(JRoute::_(K2HelperRoute::getCategoryRoute($row->id.':'.urlencode($row->alias))));
+            $categoryList[] = $row;
+        }
 
-        $db = JFactory::getDBO();
-        $db->setQuery("SELECT * FROM #__k2_items WHERE id = " . $k2ItemId );
-
-        $k2RawItem = $db->loadObject();
-
-        return $k2Item;
+        return $categoryList;
     }
 }
--- get/k2Item.php
+++ get/k2Item.php
@@ -19,4 +19,48 @@ class XeeoGetK2Item extends XeeoCoreSingleton
 
         return $k2Item;
     }
+
+    public function findByCategory($k2CategoryIdentifier, $limit = -1) {
+
+        $db = JFactory::getDBO();
+
+        $query = "SELECT items.* FROM #__k2_items as items ";
+
+        if (is_numeric($k2CategoryIdentifier)) {
+            $query .= "WHERE items.catid = " . $k2CategoryIdentifier;
+        } else {
+
+            $query .= "LEFT JOIN #__k2_categories as categories ON items.catid = categories.id ";
+            $query .= "WHERE categories.name = '" . $k2CategoryIdentifier . "'";
+            $query .= " AND categories.published=1  AND categories.trash=0";
+            if (K2_JVERSION != '15') {
+                $query .= " AND categories.access IN(" . implode(',', $user->getAuthorisedViewLevels()) . ")";
+                if ($mainframe->getLanguageFilter()) {
+                    $query .= " AND categories.language IN(" . $db->Quote(JFactory::getLanguage()->getTag()) . ", " . $db->Quote('*') . ")";
+                }
+            } else {
+                $query .= " AND categories.access<={$aid}";
+            }
+
+        }
+
+        if ($limit > 0) {
+            $query .= " LIMIT " . $limit;
+        }
+
+        $db->setQuery($query);
+        $k2Items = $db->loadObjectList();
+
+        $collection   = array();
+        $k2ItemHelper = XeeoHelperK2Item::getInstance();
+
+        foreach($k2Items as $k2RawItem) {
+
+            $k2Item = $k2ItemHelper->prepare($k2RawItem);
+
+            $collection[] = $k2Item;
+        }
+
+        return $collection;
+    }
 }
